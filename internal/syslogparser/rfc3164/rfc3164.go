73661f31cf0c2ea9f7fe4f52b92aab9dba6bec78
Tad Keller <tkeller@opentext.com>
2024-08-19 13:20:11 -0500

    fix panic caused by out of bounds slice index

--- internal/syslogparser/rfc3164/rfc3164.go
+++ internal/syslogparser/rfc3164/rfc3164.go
@@ -470,7 +470,7 @@ func (p *Parser) parseTag() (string, error) {
 	from := p.cursor
 
 	for {
-		if p.cursor == p.l {
+		if p.cursor >= p.l {
 			// no tag found, reset cursor for content
 			p.cursor = from
 			return "", nil
